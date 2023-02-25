761f1db13f02907c19ef50d8fca2bdbe2ec12484
Orlando Sorto <orlando.sorto9328@gmail.com>
2023-02-25 12:23:11 -0600

    Agregue cambios a apuntes git, js, css e index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 
-/*
-alert("Hola mundo")
-alert("Hola mundo")
-alert("Hola mundo 2")
-*/
+//alert("Hola mundo")
+
+nombre = "Orlando";
+nombre = "Luis";
+let apellido = "Sorto";
+//let apellido = "Gomez";
+var nombre = "Daniel";
+let numero = 7;
+let boolean = true;
+let paises = ["Mexico", true, 1];
+let persona = {
+    nombre : "Susan",
+    edad : 18,
+    apodo : ["Nena", "La wera"],
+    estadoCivil : false
+}
+let datos = [
+    ["Perro", "Gato", "Perico"],
+    ["Italia", "España", "Francia"],
+    [1, false, ["Daniel", "Liliana", 18]],
+];
+
+
+console.log(datos[3]);
+console.log(persona.apodo[1]);
+console.log(typeof(boolean));
+console.log(`Mi nombre es ${nombre}`);
+
+
+
+
+
+
+
